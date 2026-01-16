d2c1e164956d929e88eec650711bfc3f32fa7a19
dillank82 <dillankyuppi@gmail.com>
2026-01-16 16:18:18 +0500

    restructure Statkey type using array of values and as const

--- src/interfaces/Stats.ts
+++ src/interfaces/Stats.ts
@@ -1,6 +1,9 @@
+export const STAT_NAMES = ['affection', 'respect', 'trust'] as const
+
+export type StatKey = typeof STAT_NAMES[number]
+
 export type Stat = number
 
-export type StatKey = 'affection' | 'respect' | 'trust'
 
 export type Stats = {
     [key in StatKey]: Stat
