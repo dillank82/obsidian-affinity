32308a79c029557ab18fbc8f8f67ff4053d52080
dillank82 <dillankyuppi@gmail.com>
2026-01-16 16:02:15 +0500

    shorten tests and improve readability by moving test data to the module level

--- src/core/RelationshipsManager.test.ts
+++ src/core/RelationshipsManager.test.ts
@@ -3,189 +3,144 @@ import { RelationshipsManager } from "./RelationshipsManager"
 
 describe('RelationshipsManager', () => {
     const mockedSave = () => new Promise<void>((resolve) => { resolve() })
+    const createRelManager = (settings: PluginSettings) => new RelationshipsManager(settings, mockedSave)
+    const createSampleRel = (overrides = {}) => ({
+        fromChar: 'Char_A',
+        toChar: 'Char_B',
+        stats: {
+            affection: 18,
+            respect: 4,
+            trust: 8
+        },
+        ...overrides
+    })
 
     test('should create valid relation', async () => {
+        const fromCharId = 'fromchar'
+        const toCharId = 'tochar'
         const settings = {
             relationships: []
         } as unknown as PluginSettings
+        const relManager = createRelManager(settings)
 
-        const relManager = new RelationshipsManager(settings, mockedSave)
-
-        const fromChar = 'Char_A'
-        const toChar = 'Char_B'
-        await relManager.createRelation(fromChar, toChar)
-
+        await relManager.createRelation(fromCharId, toCharId)
         expect(settings.relationships.length).toBe(1)
 
         const rel = settings.relationships[0]!
 
-        expect(rel.fromChar).toBe(fromChar)
-        expect(rel.toChar).toBe(toChar)
+        expect(rel.fromChar).toBe(fromCharId)
+        expect(rel.toChar).toBe(toCharId)
         expect(rel.stats).toEqual({
             affection: relManager.initialStatsValue,
             respect: relManager.initialStatsValue,
             trust: relManager.initialStatsValue,
         })
     })
+
     test('should return a relation item in getRelation', () => {
-        const fromChar = 'Char_A'
-        const toChar = 'Char_B'
-        const sapmpleRel = {
-            fromChar: fromChar,
-            toChar: toChar,
-            stats: {
-                affection: 17,
-                respect: 10,
-                trust: 8
-            }
-        }
+        const sapmpleRel = createSampleRel()
         const settings = {
             relationships: [sapmpleRel]
         } as unknown as PluginSettings
 
-        const relManager = new RelationshipsManager(settings, mockedSave)
-        const rel = relManager.getRelation(fromChar, toChar)
+        const relManager = createRelManager(settings)
+        const rel = relManager.getRelation(sapmpleRel.fromChar, sapmpleRel.toChar)
         expect(rel).toEqual(sapmpleRel)
     })
+
     test('should correctly udate relation in updateAffinity', () => {
-        const fromChar = 'Char_A'
-        const toChar = 'Char_B'
-        const startStats = {
-            affection: 17,
-            respect: 10,
-            trust: 8
-        }
-        const rel = {
-            fromChar: fromChar,
-            toChar: toChar,
-            stats: {
-                affection: startStats.affection,
-                respect: startStats.respect,
-                trust: startStats.trust
-            }
-        }
+        const rel = createSampleRel()
+        const startStats = rel.stats
         const settings = {
             relationships: [rel]
         } as unknown as PluginSettings
 
-        const relManager = new RelationshipsManager(settings, mockedSave)
+        const relManager = createRelManager(settings)
 
         const deltaStats = {
-            affection: 1,
-            respect: -1,
-            trust: 1
+            affection: -1,
+            respect: 1,
+            trust: 4
         }
-        relManager.updateAffinity(
-            fromChar,
-            toChar,
-            deltaStats
-        )
+        relManager.updateAffinity(rel.fromChar, rel.toChar, deltaStats)
 
-        expect(rel.stats.affection).toBe(startStats.affection + deltaStats.affection)
-        expect(rel.stats.respect).toBe(startStats.respect + deltaStats.respect)
-        expect(rel.stats.trust).toBe(startStats.trust + deltaStats.trust)
+        expect(rel.stats).toEqual({
+            affection: startStats.affection + deltaStats.affection,
+            respect: startStats.respect + deltaStats.respect,
+            trust: startStats.trust + deltaStats.trust,
+        })
     })
-    test('should throw error when try to create realtion with existing direction', async() => {
-        const fromChar = 'Char_A'
-        const toChar = 'Char_B'
-        const rel = {
-            fromChar: fromChar,
-            toChar: toChar,
-            stats: {}
-        }
+
+    test('should throw error when try to create realtion with existing direction', async () => {
+        const rel = createSampleRel()
         const settings = {
             relationships: [rel]
         } as unknown as PluginSettings
 
-        const relManager = new RelationshipsManager(settings, mockedSave)
-        expect(await relManager.createRelation(fromChar, toChar)).toThrow('This relation already exists')
+        const relManager = createRelManager(settings)
+        expect(await relManager.createRelation(rel.fromChar, rel.toChar)).toThrow('This relation already exists')
     })
-    test('should throw error when fromChar === toChar', () => {
-        const fromChar = 'Char_A'
-        const toChar = fromChar
-
+    
+    test('should throw error when from and char ID are same', () => {
+        const from = 'A'
+        const to = from
         const settings = {} as unknown as PluginSettings
 
-        const relManager = new RelationshipsManager(settings, mockedSave)
+        const relManager = createRelManager(settings)
         const error = '2 different characters must be specified'
 
-        expect(relManager.createRelation(fromChar, toChar)).toThrow(error)
-        expect(relManager.getRelation(fromChar, toChar)).toThrow(error)
-        expect(relManager.updateAffinity(fromChar, toChar, {})).toThrow(error)
+        expect(relManager.createRelation(from, to)).toThrow(error)
+        expect(relManager.getRelation(from, to)).toThrow(error)
+        expect(relManager.updateAffinity(from, to, {})).toThrow(error)
     })
+
     test('stats must not beat min and max edges after updating', () => {
-        const fromChar = 'Char_A'
-        const toChar = 'Char_B'
-        const startStats = {
-            affection: 20,
-            respect: 1,
-            trust: 8
-        }
-        const rel = {
-            fromChar: fromChar,
-            toChar: toChar,
+        const rel = createSampleRel({
             stats: {
-                affection: startStats.affection,
-                respect: startStats.respect,
-                trust: startStats.trust
+                affection: 20,
+                respect: 1
             }
-        }
+        })
         const settings = {
             relationships: [rel]
         } as unknown as PluginSettings
 
-        const relManager = new RelationshipsManager(settings, mockedSave)
-        const res = relManager.updateAffinity(fromChar, toChar, { affection: 4, respect: -4 })
+        const relManager = createRelManager(settings)
+        const res = relManager.updateAffinity(rel.fromChar, rel.toChar, { affection: 4, respect: -4 })
         expect(res.affection).toEqual({
-            value: startStats.affection,
+            value: 20,
             change: 0,
             status: 'MAXED_OUT'
         })
         expect(res.respect).toEqual({
-            value: startStats.respect,
+            value: 1,
             change: 0,
             status: 'MINED_OUT'
         })
     })
-    test('should return null when trying get non-existing relation', () => {
-        const fromChar = 'Char_A'
-        const toChar = 'Char_B'
-        const settings = {} as unknown as PluginSettings
 
-        const relManager = new RelationshipsManager(settings, mockedSave)
-        const rel = relManager.getRelation(fromChar, toChar)
+    test('should return null when trying get non-existing relation', () => {
+        const settings = {} as unknown as PluginSettings
+        const relManager = createRelManager(settings)
+        const rel = relManager.getRelation('fromCharId', 'toCharId')
         expect(rel).toBeNull()
     })
+
     test('updateAffinity should work correctly with delta === 0', () => {
-        const fromChar = 'Char_A'
-        const toChar = 'Char_B'
-        const startStats = {
-            affection: 20,
-            respect: 1,
-            trust: 8
-        }
-        const rel = {
-            fromChar: fromChar,
-            toChar: toChar,
-            stats: {
-                affection: startStats.affection,
-                respect: startStats.respect,
-                trust: startStats.trust
-            }
-        }
+        const rel = createSampleRel()
+        const startStats = rel.stats
         const settings = {
             relationships: [rel]
         } as unknown as PluginSettings
 
-        const relManager = new RelationshipsManager(settings, mockedSave)
+        const relManager = createRelManager(settings)
 
         const res = relManager.updateAffinity(
-            fromChar, toChar, {
-                affection: 0,
-                respect: 0, 
-                trust: 0
-            }
-        )
+            rel.fromChar, rel.toChar, {
+            affection: 0,
+            respect: 0,
+            trust: 0
+        })
         expect(res.affection).toEqual({
             value: startStats.affection,
             change: 0,
@@ -202,33 +157,20 @@ describe('RelationshipsManager', () => {
             status: 'UNCHANGED'
         })
     })
+
     test('updateAffinity should work correctly with partial set of stats', () => {
-        const fromChar = 'Char_A'
-        const toChar = 'Char_B'
-        const startStats = {
-            affection: 20,
-            respect: 1,
-            trust: 8
-        }
-        const rel = {
-            fromChar: fromChar,
-            toChar: toChar,
-            stats: {
-                affection: startStats.affection,
-                respect: startStats.respect,
-                trust: startStats.trust
-            }
-        }
+        const rel = createSampleRel()
+        const startStats = rel.stats
         const settings = {
             relationships: [rel]
         } as unknown as PluginSettings
 
-        const relManager = new RelationshipsManager(settings, mockedSave)
+        const relManager = createRelManager(settings)
         const delta = {
             respect: 1,
             trust: -2
         }
-        const res = relManager.updateAffinity(fromChar, toChar, delta)
+        const res = relManager.updateAffinity(rel.fromChar, rel.toChar, delta)
 
         expect(res.affection).toEqual({
             value: startStats.affection,
@@ -246,32 +188,17 @@ describe('RelationshipsManager', () => {
             status: 'CHANGED'
         })
     })
+
     test('updateAffinity should work correctly with empty set of stats', () => {
-        const fromChar = 'Char_A'
-        const toChar = 'Char_B'
-        const startStats = {
-            affection: 20,
-            respect: 1,
-            trust: 8
-        }
-        const rel = {
-            fromChar: fromChar,
-            toChar: toChar,
-            stats: {
-                affection: startStats.affection,
-                respect: startStats.respect,
-                trust: startStats.trust
-            }
-        }
+        const rel = createSampleRel()
+        const startStats = rel.stats
         const settings = {
             relationships: [rel]
         } as unknown as PluginSettings
 
-        const relManager = new RelationshipsManager(settings, mockedSave)
+        const relManager = createRelManager(settings)
 
-        const res = relManager.updateAffinity(
-            fromChar, toChar, {}
-        )
+        const res = relManager.updateAffinity(rel.fromChar, rel.toChar, {})
         expect(res.affection).toEqual({
             value: startStats.affection,
             change: 0,
@@ -288,13 +215,10 @@ describe('RelationshipsManager', () => {
             status: 'UNCHANGED'
         })
     })
+    
     test('should throw error when trying to update non-existing relation', () => {
-        const fromChar = 'Char_A'
-        const toChar = 'Char_B'
         const settings = {} as unknown as PluginSettings
-
-        const relManager = new RelationshipsManager(settings, mockedSave)
-
-        expect(relManager.updateAffinity(fromChar, toChar, {})).toThrow('Relation not found')
+        const relManager = createRelManager(settings)
+        expect(relManager.updateAffinity('fromCharId', 'toCharId', {})).toThrow('Relation not found')
     })
 })
