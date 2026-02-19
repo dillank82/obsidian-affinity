2c74c4350838c549d0a11cdb32572221723cb6be
dillank82 <dillankyuppi@gmail.com>
2026-02-19 13:24:45 +0500

    feat: implement ChangeAffinityForm in AffinityDashboard

---
+++ src/components/ChangeAffinityForm/ChangeAffinityForm.tsx
@@ -0,0 +1,39 @@
+import { StatChanger } from "components/StatChanger/StatChanger";
+import { Stats } from "interfaces/Stats";
+import { FC, FormEvent } from "react";
+import { rollDice } from "utils/rollDice";
+
+interface ChangeAffinityFormProps {
+    updateAffinity: (delta: Partial<Stats>) => void
+}
+
+export const ChangeAffinityForm: FC<ChangeAffinityFormProps> = ({ updateAffinity }) => {
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+
+        const resolveValue = (value: FormDataEntryValue | null): number => {
+            if (value === '1d4') return rollDice(4)
+            return Number(value) || 0
+        }
+        const delta: Partial<Stats> = {
+            affection: resolveValue(formData.get('affection')),
+            respect: resolveValue(formData.get('respect')),
+            trust: resolveValue(formData.get('trust'))
+        }
+
+        updateAffinity(delta)
+    }
+    return (
+        <form
+            id="change-affinity-form"
+            onSubmit={handleSubmit}    
+        >
+            <label htmlFor="change-affinity-form">Choose affinity changes</label>
+            <StatChanger label="Affection" name="affection" />
+            <StatChanger label="Respect" name="respect" />
+            <StatChanger label="Trust" name="trust" />
+            <button type="submit">Submit</button>
+        </form>
+    )
+}
