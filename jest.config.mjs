00edf37cd0e10f13eaf5e1e52f3e54bb3748b4ae
dillank82 <dillankyuppi@gmail.com>
2026-02-24 11:32:03 +0500

    chore: configure jest-css-modules-transformer

--- jest.config.mjs
+++ jest.config.mjs
@@ -2,8 +2,8 @@ const config = {
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
     setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-    moduleNameMapper: {
-        '.module.(css|sass|scss)$': 'identity-obj-proxy'
+    transform: {
+        ".+\\.(css|styl|less|sass|scss)$": "jest-css-modules-transform"
     }
 }
 
