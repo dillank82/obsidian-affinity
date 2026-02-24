43402f4878fe81f56b4d244dea7f5dfc1a017372
dillank82 <dillankyuppi@gmail.com>
2026-02-24 12:55:33 +0500

    chore: add moduleDirectories to jest config

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,10 +1,11 @@
 const config = {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-    transform: {
-        ".+\\.(css|styl|less|sass|scss)$": "jest-css-modules-transform"
-    }
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  moduleDirectories: ['node_modules', 'src'],
+  transform: {
+    ".+\\.(css|styl|less|sass|scss)$": "jest-css-modules-transform"
+  }
 }
 
 export default config
