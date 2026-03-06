21199d6f014e26f80283b32825bcaa2c10fba355
dillank82 <dillankyuppi@gmail.com>
2026-03-06 15:26:50 +0500

    refactor: remove unused sample code and Actions interface

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { App, debounce, Editor, FrontMatterCache, MarkdownView, Modal, Notice, Plugin, TFile, WorkspaceLeaf } from 'obsidian'
-import { AffinitySettingsTab, DEFAULT_SETTINGS, PluginSettings } from "./settings"
+import { debounce, FrontMatterCache, Plugin, TFile } from 'obsidian'
+import { DEFAULT_SETTINGS, PluginSettings } from "./settings"
 import { AffinityProcessor } from 'processors/AffinityProcessor';
 import { CharacterID } from 'interfaces/Realtionships';
 import { useStore } from 'store';
@@ -29,61 +29,6 @@ export default class AffinityPlugin extends Plugin {
 			const id = await this.getAffinityId(this.app.workspace.getActiveFile())
 			await this.processor.process(source, el, ctx, this.relManager, id)
 		})
-
-		this.addRibbonIcon('dice', 'Activate view', async (evt: MouseEvent) => { })
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status bar text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-modal-simple',
-			name: 'Open modal (simple)',
-			callback: () => {
-				new AffinityModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				editor.replaceSelection('Sample editor command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-modal-complex',
-			name: 'Open modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new AffinityModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-				return false;
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new AffinitySettingsTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			new Notice("Click");
-		});
-	}
-
-	onunload() {
 	}
 
 	async loadSettings() {
@@ -92,19 +37,6 @@ export default class AffinityPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-
-	async activateView(viewType: string) {
-		const { workspace } = this.app
-		let leaf: WorkspaceLeaf | null = null
-		const leaves = workspace.getLeavesOfType(viewType)
-		if (leaves.length > 0) {
-			leaf = leaves[0] || null
-		} else {
-			leaf = workspace.getRightLeaf(false)
-			await leaf?.setViewState({ type: viewType, active: true })
-		}
-		if (leaf) await workspace.revealLeaf(leaf)
 	}
 
 	private getFrontmatter(file: TFile | null): FrontMatterCache | null {
@@ -138,21 +70,5 @@ export default class AffinityPlugin extends Plugin {
 		} catch (e) {
 			console.error("Frontmatter update error:", e)
 		}
-	}
-}
-
-class AffinityModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let { contentEl } = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
 	}
 }
--- src/settings.ts
+++ src/settings.ts
@@ -1,39 +1,9 @@
-import {App, PluginSettingTab, Setting} from "obsidian";
-import MyPlugin from "./main";
 import { Relationships } from "interfaces/Realtionships";
 
 export interface PluginSettings {
-	mySetting: string;
 	relationships: Relationships
 }
 
 export const DEFAULT_SETTINGS: PluginSettings = {
-	mySetting: 'default',
 	relationships: {}
-}
-
-export class AffinitySettingsTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Settings #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
 }
