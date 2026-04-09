11113669b2cc5049b65d1628b4159eb04cf99905
dillank82 <dillankyuppi@gmail.com>
2026-04-09 12:03:16 +0500

    fix(header): make toChar optional and add text for its absence

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import { FC } from "react"
 import styles from './Header.module.css'
 
 interface HeaderProps {
-    toChar: string
+    toChar: string | null
     setToChar: (value: string) => void
     charOptions: string[]
 }
@@ -11,7 +11,7 @@ interface HeaderProps {
 export const Header: FC<HeaderProps> = ({ toChar, setToChar, charOptions }) => {
     return (
         <header className={styles.header}>
-            <h1>Relation to {toChar}</h1>
+            <h1>{ toChar ? `Relation to ${toChar}` : 'Affinity' }</h1>
             <CharacterSwitcher currentChar={toChar} onChange={setToChar} options={charOptions} />
         </header>
     )
