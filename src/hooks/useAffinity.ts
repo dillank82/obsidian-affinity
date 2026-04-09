5f796de178f1123081dfb796953652010e4b9550
dillank82 <dillankyuppi@gmail.com>
2026-04-09 10:04:52 +0500

    refactor(useAffinity): improve types predictability of return object using discriminated unions

--- src/hooks/useAffinity.ts
+++ src/hooks/useAffinity.ts
@@ -1,15 +1,60 @@
 import { CharacterID } from "interfaces/Realtionships"
-import { Stats } from "interfaces/Stats"
+import { Stats, StatsLabels } from "interfaces/Stats"
 import { useState, useMemo } from "react"
 import { Store } from "store"
 import { mapStats } from "utils/mapStats"
 
-export const useAffinity = (store: Store, fromChar: CharacterID) => {
-    const [toChar, setToChar] = useState<string | null>(null)
+interface UseAffinityItemsInitial {
+    stats: null
+    labels: null
+    updateAffinity: null
+}
 
+interface UseAffinityItemsChosen {
+    stats: Stats | null
+    labels: StatsLabels | null
+    updateAffinity: (delta: Partial<Stats>) => void 
+}
+
+type UseAffinityInitial = UseAffinityItemsInitial & UseAffinityStateInitial
+type UseAffinityChosen = UseAffinityItemsChosen & UseAffinityStateChosen
+type UseAffinity = UseAffinityInitial | UseAffinityChosen
+
+type UseAffinityStateInitial = { status: 'initial', toChar: null }
+type UseAffinityStateChosen = { status: 'chosen', toChar: string }
+
+type UseAffinityState = UseAffinityStateChosen | UseAffinityStateInitial
+
+type UseAffinityReturn = UseAffinity & {
+    setToChar: (toChar: CharacterID) => void
+    relOptions: string[]
+}
+
+export const useAffinity = (store: Store, fromChar: CharacterID): UseAffinityReturn => {
+    const [state, setState] = useState<UseAffinityState>({ status: 'initial', toChar: null })
+    const setToChar = (toChar: CharacterID) => {
+        setState({
+            status: 'chosen',
+            toChar: toChar
+        })
+    }
+
+    if (state.status === 'initial') {
+        return {
+            status: state.status,
+            toChar: null,
+            stats: null,
+            labels: null,
+            updateAffinity: null,
+            relOptions: [],
+            setToChar
+        }
+    }
+
+    const toChar = state.toChar
     const relations = store.relationships[fromChar]
-    const stats = (toChar ? relations?.[toChar] : null) || null
-    
+    const stats = relations?.[toChar] || null
+
     const relOptions = Object.keys(relations || {})
 
     const labels = useMemo(() => {
@@ -17,9 +62,9 @@ export const useAffinity = (store: Store, fromChar: CharacterID) => {
         return mapStats(stats)
     }, [stats])
 
-    const updateAffinity = toChar ? (delta: Partial<Stats>) => {
+    const updateAffinity = (delta: Partial<Stats>) => {
         store.updateRelation(fromChar, toChar, delta)
-    } : null
+    }
 
-    return { toChar, setToChar, stats, labels, updateAffinity, relOptions }
+    return { status: state.status, toChar, setToChar, stats, labels, updateAffinity, relOptions }
 }
