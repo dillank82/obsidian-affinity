50937334024f8c580f49e96823d0d319c606add3
Evgeny <dillank82@gmail.com>
2026-04-15 10:03:02 +0500

    refactor(types): remove unused Direction and RelationshipsItem interfaces

--- src/interfaces/Realtionships.ts
+++ src/interfaces/Realtionships.ts
@@ -7,15 +7,6 @@ export interface Character {
     name: string
 }
 
-export interface Direction {
-    fromChar: string
-    toChar: string
-}
-
-export interface RelationshipsItem extends Direction {
-    stats: Stats
-}
-
 type fromChar = CharacterID
 type toChar = CharacterID
 export type Relationships = Record<fromChar, Record<toChar, Stats>>
