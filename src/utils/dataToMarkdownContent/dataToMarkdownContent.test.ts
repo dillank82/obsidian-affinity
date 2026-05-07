a9889357efea82faeeef028347b7f41ade8ce593
Evgeny Savelyev <dillank82@gmail.com>
2026-05-07 09:52:23 +0500

    test(dataToMarkdownContent): add missing toChar case

--- src/utils/dataToMarkdownContent/dataToMarkdownContent.test.ts
+++ src/utils/dataToMarkdownContent/dataToMarkdownContent.test.ts
@@ -1,17 +1,26 @@
 import { dataToMarkdownContent } from "./dataToMarkdownContent"
 
 describe('dataToMarkdownContent', () => {
-    it('should return correct structure', () => {
-        expect(
-  dataToMarkdownContent({
-    id: 'bb357792-c996-40cd-ab9e-ed78ae623ef5',
-    toCharId: 'bf5337ad-b689-4d0c-9048-1a44ffbe35e9'
-  })
-).toMatchInlineSnapshot(`
+
+  it('should return correct structure', () => {
+    expect(
+      dataToMarkdownContent({
+        id: 'bb357792-c996-40cd-ab9e-ed78ae623ef5',
+        toCharId: 'bf5337ad-b689-4d0c-9048-1a44ffbe35e9'
+      })
+    ).toMatchInlineSnapshot(`
 "\`\`\`affinity
   id: bb357792-c996-40cd-ab9e-ed78ae623ef5
   toChar: bf5337ad-b689-4d0c-9048-1a44ffbe35e9
 \`\`\`"
 `)
-    })
+  })
+
+  it('should correctly handle missing toChar', () => {
+    expect(dataToMarkdownContent({ id: 'bb357792-c996-40cd-ab9e-ed78ae623ef5' })).toMatchInlineSnapshot(`
+"\`\`\`affinity
+  id: bb357792-c996-40cd-ab9e-ed78ae623ef5
+\`\`\`"
+`)
+  })
 })
