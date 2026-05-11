1995d5cfc3a781dadf76ef1c458d1754dbc2eef1
Evgeny Savelyev <dillank82@gmail.com>
2026-05-11 10:38:14 +0500

    feat(core): add command to insert plugin code block into a note

---
+++ src/commands.ts
@@ -0,0 +1,21 @@
+import AffinityPlugin from "main"
+import { MarkdownView, Notice } from "obsidian"
+import { dataToMarkdownContent } from "utils/dataToMarkdownContent/dataToMarkdownContent"
+import { generateId } from "utils/generateId"
+
+export const addCommands = (plugin: AffinityPlugin) => {
+    plugin.addCommand({
+        id: 'create-code-block',
+        name: 'Insert relationships tracker',
+        callback: () => {
+            const editor = plugin.app.workspace.getActiveViewOfType(MarkdownView)?.editor
+            if (!editor) {
+                new Notice('Open a note to insert the tracker.')
+                return
+            }
+
+            const codeBlock = dataToMarkdownContent({ id: generateId() })
+            editor.replaceSelection(codeBlock)
+        }
+    })
+}
