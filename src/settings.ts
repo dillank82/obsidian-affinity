c4e1ec6fbcabeb4d9f7fabf2628a811ca182b30b
Evgeny Savelyev <dillank82@gmail.com>
2026-05-11 11:45:33 +0500

    feat(settings): add charactersDirectory to PluginSettings interface

--- src/settings.ts
+++ src/settings.ts
@@ -2,8 +2,10 @@ import { Relationships } from "interfaces/Realtionships";
 
 export interface PluginSettings {
 	relationships: Relationships
+	charactersDirectory: string
 }
 
 export const DEFAULT_SETTINGS: PluginSettings = {
-	relationships: {}
+	relationships: {},
+	charactersDirectory: ''
 }
