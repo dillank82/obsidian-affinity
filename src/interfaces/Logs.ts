67995ec830c6d8216e4ad8f35bbc16960f03bdbf
Evgeny Savelyev <dillank82@gmail.com>
2026-05-20 11:29:10 +0500

    feat(logs): add log interface

---
+++ src/interfaces/Logs.ts
@@ -0,0 +1,11 @@
+import { CharacterID } from "./Realtionships";
+import { Stats } from "./Stats";
+
+export interface Log {
+    changes: Partial<Stats>
+    // temporarily optional to implement core logic before commenting
+    cause?: string
+    timestamp: string
+}
+
+export type LogsHistory = Record<CharacterID, Log[]>
