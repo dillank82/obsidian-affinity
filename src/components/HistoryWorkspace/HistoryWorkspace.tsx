cf0a9667d0c44a5b9c16097b6c7d6b791c4316a1
Evgeny Savelyev <dillank82@gmail.com>
2026-05-25 10:46:24 +0500

    feat(HistoryWorkspace): implement logs table

--- src/components/HistoryWorkspace/HistoryWorkspace.tsx
+++ src/components/HistoryWorkspace/HistoryWorkspace.tsx
@@ -6,7 +6,40 @@ interface HistoryWorkspaceProps {
 }
 
 export const HistoryWorkspace: FC<HistoryWorkspaceProps> = ({ logsHistory }) => {
+    const sortedLogs = [...logsHistory].sort((a, b) => new Date(a.timestamp).getTime() - new Date(b.timestamp).getTime())
+
     return(
-        <></>
+        <table>
+            <thead>
+                <tr>
+                    <th>
+                        A|R|T
+                    </th>
+                    <th>
+                        Cause
+                    </th>
+                    <th>
+                        Timestamp
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                {sortedLogs.map((log) => (
+                    <tr key={log.timestamp}>
+                        <td>
+                            <span>{log.changes.affection || '-'}</span>
+                            <span>{log.changes.respect || '-'}</span>
+                            <span>{log.changes.trust || '-'}</span>
+                        </td>
+                        <td>
+                            {log.cause || '-'}
+                        </td>
+                        <td>
+                            {log.timestamp}
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
     )
 }
