a10ae1d284ab5de58613968d404bd07ec9a5cc5e
Evgeny Savelyev <dillank82@gmail.com>
2026-05-25 11:12:37 +0500

    feat(HistoryWorkspace): normalize changes display

--- src/components/HistoryWorkspace/HistoryWorkspace.tsx
+++ src/components/HistoryWorkspace/HistoryWorkspace.tsx
@@ -7,6 +7,11 @@ interface HistoryWorkspaceProps {
 
 export const HistoryWorkspace: FC<HistoryWorkspaceProps> = ({ logsHistory }) => {
     const sortedLogs = [...logsHistory].sort((a, b) => new Date(a.timestamp).getTime() - new Date(b.timestamp).getTime())
+    const normalizeChangesValue = (value: number | undefined): string => {
+        if (!value) return '-'
+        else if (value > 0) return `+${value}`
+        else return String(value)
+    }
 
     return(
         <table>
@@ -27,9 +32,9 @@ export const HistoryWorkspace: FC<HistoryWorkspaceProps> = ({ logsHistory }) => 
                 {sortedLogs.map((log) => (
                     <tr key={log.timestamp}>
                         <td>
-                            <span>{log.changes.affection || '-'}</span>
-                            <span>{log.changes.respect || '-'}</span>
-                            <span>{log.changes.trust || '-'}</span>
+                            <span>{normalizeChangesValue(log.changes.affection)}</span>
+                            <span>{normalizeChangesValue(log.changes.respect)}</span>
+                            <span>{normalizeChangesValue(log.changes.trust)}</span>
                         </td>
                         <td>
                             {log.cause || '-'}
