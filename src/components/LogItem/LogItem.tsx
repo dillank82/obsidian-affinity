991d0399d05f4766dd4f9bc91cf3dbbf4424caaa
Evgeny Savelyev <dillank82@gmail.com>
2026-06-01 10:42:54 +0500

    feat(LogItem): add obsidian tooltips to changes

--- src/components/LogItem/LogItem.tsx
+++ src/components/LogItem/LogItem.tsx
@@ -1,5 +1,6 @@
-import { FC } from "react"
+import { FC, useEffect, useRef } from "react"
 import styles from './LogItem.module.css'
+import { setTooltip } from "obsidian"
 
 interface LogItemProps {
     affection: {
@@ -19,19 +20,28 @@ interface LogItemProps {
 }
 
 export const LogItem: FC<LogItemProps> = ({ affection, respect, trust, timestamp, cause }) => {
+    const affectionRef = useRef<HTMLElement>(null)
+    const respectRef = useRef<HTMLElement>(null)
+    const trustRef = useRef<HTMLElement>(null)
+
+    useEffect(() => {
+        if (affectionRef.current) setTooltip(affectionRef.current, 'Affection')
+        if (respectRef.current) setTooltip(respectRef.current, 'Respect')
+        if (trustRef.current) setTooltip(trustRef.current, 'Trust')
+    }, [])
 
     return (
         <div className={styles.container}>
             <span className={styles.text}>{cause}</span>
 
             <div className={styles.changesWrapper}>
-                <span className={styles[affection.changeType]}>
+                <span className={styles[affection.changeType]} ref={affectionRef}>
                     {affection.value}
                 </span>
-                <span className={styles[respect.changeType]}>
+                <span className={styles[respect.changeType]} ref={respectRef}>
                     {respect.value}
                 </span>
-                <span className={styles[trust.changeType]}>
+                <span className={styles[trust.changeType]} ref={trustRef}>
                     {trust.value}
                 </span>
             </div>
