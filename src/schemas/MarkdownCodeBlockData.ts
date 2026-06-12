e48677b0766a3a05f91ffaee80325378bc1dc7a3
Evgeny Savelyev <dillank82@gmail.com>
2026-06-12 14:22:42 +0500

    fix(schema): replace optional with nullable

--- src/schemas/MarkdownCodeBlockData.ts
+++ src/schemas/MarkdownCodeBlockData.ts
@@ -2,6 +2,6 @@ import z from "zod";
 
 export const MarkdownCodeBlockDataSchema = z.object({
     id: z.uuid(),
-    toCharId: z.uuid().optional()
+    toCharId: z.uuid().nullable()
 })
 export type MarkdownCodeBlockData = z.infer<typeof MarkdownCodeBlockDataSchema>
