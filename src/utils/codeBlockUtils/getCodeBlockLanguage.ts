be03b720c51fea43901555cd32334df20366a366
Evgeny Savelyev <dillank82@gmail.com>
2026-06-12 15:08:43 +0500

    feat(getCodeBlockLanguage): add starts with triple backticks check

--- src/utils/codeBlockUtils/getCodeBlockLanguage.ts
+++ src/utils/codeBlockUtils/getCodeBlockLanguage.ts
@@ -1,1 +1,5 @@
-export const getCodeBlockLanguage = (block: string) => block.split('\n')[0]?.slice(3)
+export const getCodeBlockLanguage = (block: string) => {
+    if (block.startsWith("```")) {
+        return block.split('\n')[0]!.slice(3)
+    } else return ''
+}
