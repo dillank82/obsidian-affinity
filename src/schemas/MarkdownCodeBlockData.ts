c11faa30a17213c983e7431e89c700dd87863670
Evgeny Savelyev <dillank82@gmail.com>
2026-06-15 08:51:07 +0500

    fix(schema): replace nullable with combination of nullish and transform

--- src/schemas/MarkdownCodeBlockData.ts
+++ src/schemas/MarkdownCodeBlockData.ts
@@ -2,6 +2,6 @@ import z from "zod";
 
 export const MarkdownCodeBlockDataSchema = z.object({
     id: z.uuid(),
-    toCharId: z.uuid().nullable()
+    toCharId: z.uuid().nullish().transform(val => val === undefined ? null : val)
 })
 export type MarkdownCodeBlockData = z.infer<typeof MarkdownCodeBlockDataSchema>
