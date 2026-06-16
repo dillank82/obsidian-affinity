b47c4df138ee928e33af3b2aaf28702406def89c
Evgeny Savelyev <dillank82@gmail.com>
2026-06-16 15:13:52 +0500

    refactor(AST-iteration): extract iterator from inlineLivePreview and implement it to WriteService

--- src/services/WriteService/WriteService.ts
+++ src/services/WriteService/WriteService.ts
@@ -1,50 +1,33 @@
-import { markdownLanguage } from "@codemirror/lang-markdown";
 import { EditorState } from "@codemirror/state";
 import { Editor, EditorPosition } from "obsidian";
-import { MarkdownCodeBlockData, MarkdownCodeBlockDataSchema } from "schemas/MarkdownCodeBlockData";
+import { MarkdownCodeBlockData } from "schemas/MarkdownCodeBlockData";
 import { dataToMarkdownContent } from "utils/dataToMarkdownContent/dataToMarkdownContent";
-import { parseYamlObsidian } from "utils/obsidianParser";
+import { AffinityNodeData, iterateAffinityBlocks } from "utils/iterateAffinityBlocks";
 
 export const findBlockRanges = (state: EditorState, id: string): { from: EditorPosition, to: EditorPosition } | null => {
-    const tree = markdownLanguage.parser.parse(state.doc.toString())
-
     let ranges: { from: EditorPosition, to: EditorPosition } | null = null
+    const setRanges = (data: AffinityNodeData) => {
+        const foundedId = data.id
+        if (id === foundedId) {
+            const { from, to } = data
+            const fromLine = state.doc.lineAt(from)
+            const toLine = state.doc.lineAt(to)
+            ranges = {
+                from: {
+                    line: fromLine.number - 1,
+                    ch: from - fromLine.from
+                },
+                to: {
+                    line: toLine.number - 1,
+                    ch: to - toLine.from
 
-    tree.iterate({
-        enter: (node) => {
-            if (node.name === "FencedCode") {
-                const doc = state.doc
-                const infoStringNode = node.node.getChild("CodeInfo")
-                if (infoStringNode) {
-                    const lang = doc.sliceString(infoStringNode.from, infoStringNode.to)
-                    if (lang === "affinity") {
-                        const codeTextNode = node.node.getChild("CodeText")
-                        if (codeTextNode) {
-                            const codeText = doc.sliceString(codeTextNode?.from, codeTextNode?.to)
-                            const rawData = parseYamlObsidian(codeText)
-                            const data = MarkdownCodeBlockDataSchema.parse(rawData)
-                            if (data.id === id) {
-                                const fromLine = doc.lineAt(node.from)
-                                const toLine = doc.lineAt(node.to)
-                                ranges = {
-                                    from: {
-                                        line: fromLine.number - 1,
-                                        ch: node.from - fromLine.from
-                                    },
-                                    to: {
-                                        line: toLine.number - 1,
-                                        ch: node.to - toLine.from
-                                    }
-                                }
-                                return false
-                            }
-                        }
-                    }
                 }
+
             }
-            return
         }
-    })
+    }
+
+    iterateAffinityBlocks(state, setRanges)
     return ranges
 }
 
