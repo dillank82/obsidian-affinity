5476881db078c20cb546e85a4ff5ed56c30839da
Evgeny Savelyev <dillank82@gmail.com>
2026-06-16 13:58:28 +0500

    refactor(inlineLivePreview): extract decoration building into an external function

--- src/inlineLivePreview.ts
+++ src/inlineLivePreview.ts
@@ -14,38 +14,7 @@ export const affinityField = (containerEl: HTMLElement, app: App, fromCharId: ()
     },
     update(oldState: DecorationSet, transaction: Transaction): DecorationSet {
         if (!transaction.docChanged && oldState !== Decoration.none) return oldState
-
-        const builder = new RangeSetBuilder<Decoration>()
-        const tree = syntaxTree(transaction.state)
-
-        let from: number | null
-
-        tree.iterate({
-            enter(node) {
-                const doc = transaction.state.doc
-                if (node.name.includes('HyperMD-codeblock-begin')) {
-                    const lang = getCodeBlockLanguage(doc.sliceString(node.from, node.to))
-                    if (lang === "affinity") from = node.from
-                    else from = null
-                } else if (node.name.includes('HyperMD-codeblock-end')) {
-                    if (from === null) return
-                    const codeBlock = doc.sliceString(from, node.to)
-                    const rawData = extractCodeBlockData(codeBlock)
-                    const { id, toCharId } = validateCodeBlockData(rawData, parseYaml, console.error)
-                    builder.add(
-                        from,
-                        node.to,
-                        Decoration.replace({
-                            widget: new AffinityWidget(containerEl, app, id, fromCharId(), toCharId),
-                            block: true
-                        })
-                    )
-                    from = null
-                }
-            }
-        })
-
-        return builder.finish()
+        return buildDecorations(transaction, containerEl, app, fromCharId)
     },
     provide(field: StateField<DecorationSet>): Extension {
         return [
@@ -54,3 +23,37 @@ export const affinityField = (containerEl: HTMLElement, app: App, fromCharId: ()
         ]
     }
 })
+
+const buildDecorations = (transaction: Transaction, containerEl: HTMLElement, app: App, fromCharId: () => CharacterID): DecorationSet => {
+    const builder = new RangeSetBuilder<Decoration>()
+    const tree = syntaxTree(transaction.state)
+
+    let from: number | null
+
+    tree.iterate({
+        enter(node) {
+            const doc = transaction.state.doc
+            if (node.name.includes('HyperMD-codeblock-begin')) {
+                const lang = getCodeBlockLanguage(doc.sliceString(node.from, node.to))
+                if (lang === "affinity") from = node.from
+                else from = null
+            } else if (node.name.includes('HyperMD-codeblock-end')) {
+                if (from === null) return
+                const codeBlock = doc.sliceString(from, node.to)
+                const rawData = extractCodeBlockData(codeBlock)
+                const { id, toCharId } = validateCodeBlockData(rawData, parseYaml, console.error)
+                builder.add(
+                    from,
+                    node.to,
+                    Decoration.replace({
+                        widget: new AffinityWidget(containerEl, app, id, fromCharId(), toCharId),
+                        block: true
+                    })
+                )
+                from = null
+            }
+        }
+    })
+
+    return builder.finish()
+}
