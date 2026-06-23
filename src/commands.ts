ecf3531821a8a363f09dc3512e8dd492ef5e8a98
Evgeny Savelyev <dillank82@gmail.com>
2026-06-23 16:28:39 +0500

    feat(focus-command): hide command from palette if no widgets in note

--- src/commands.ts
+++ src/commands.ts
@@ -1,7 +1,9 @@
+import { EditorView } from "@codemirror/view"
 import AffinityPlugin from "main"
 import { MarkdownView, Notice } from "obsidian"
 import { dataToMarkdownContent } from "utils/dataToMarkdownContent/dataToMarkdownContent"
 import { generateId } from "utils/generateId"
+import { iterateAffinityBlocks } from "utils/iterateAffinityBlocks"
 import { widgetRegistry } from "widgetFocusRegistry"
 
 export const addCommands = (plugin: AffinityPlugin) => {
@@ -26,6 +28,15 @@ export const addCommands = (plugin: AffinityPlugin) => {
         checkCallback: (checking) => {
             const view = plugin.app.workspace.getActiveViewOfType(MarkdownView)
             if (!view) return false
+            
+            let docHasWidget: boolean = false
+            const editor = view.editor
+            // A necessary hack to gain access to the private CodeMirror API
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
+            const editorView = (editor as any).cm as EditorView
+            iterateAffinityBlocks(editorView.state, () => { if (!docHasWidget) docHasWidget = true })
+            if (!docHasWidget) return false
+
             if (!checking) widgetRegistry.focusNext(view.file!.path)
             return true
         }
