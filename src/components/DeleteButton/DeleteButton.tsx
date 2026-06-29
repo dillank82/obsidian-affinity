8b69fa7e558f31900be2a1064a046124adcea99d
Evgeny Savelyev <dillank82@gmail.com>
2026-06-29 12:23:32 +0500

    refactor(DeleteButton): add mode split to deleteWidget function

--- src/components/DeleteButton/DeleteButton.tsx
+++ src/components/DeleteButton/DeleteButton.tsx
@@ -2,7 +2,7 @@ import { EditorView } from "@codemirror/view"
 import { useApp } from "context"
 import { MarkdownView, Notice } from "obsidian"
 import { FC } from "react"
-import { deleteMarkdownContent, findBlockRanges } from "services/WriteService/WriteService"
+import { appVaultWriter, editorWriter } from "services/WriteService/WriteService"
 import { X } from "lucide-react"
 
 interface DeleteButtonProps {
@@ -11,9 +11,14 @@ interface DeleteButtonProps {
 
 export const DeleteButton: FC<DeleteButtonProps> = ({ codeBlockId }) => {
     const app = useApp()
-    const deleteWidget = () => {
+    const deleteWidget = async () => {
         try {
-            const editor = app.workspace.getActiveViewOfType(MarkdownView)?.editor
+            const view = app.workspace.getActiveViewOfType(MarkdownView)
+            if (!view) {
+                new Notice('Failed to save character selection. No active view.')
+                return
+            }
+            const editor = view?.editor
             // A necessary hack to gain access to the private CodeMirror API
             // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
             const editorView = (editor as any).cm as EditorView
@@ -22,16 +27,33 @@ export const DeleteButton: FC<DeleteButtonProps> = ({ codeBlockId }) => {
                 return
             }
 
-            const state = editorView.state
-            const ranges = findBlockRanges(state, codeBlockId)
-
-            if (!ranges) {
-                new Notice('Failed to delete widget: cannot find current code block range.')
-                return
+            const viewMode = view.getMode()
+            switch (viewMode) {
+                case "source": {
+                    const ranges = editorWriter.findBlockRanges(editorView.state, codeBlockId)
+                    if (!ranges) {
+                        new Notice('Failed to delete widget: cannot find current code block range.')
+                        return
+                    }
+                    const { from, to } = ranges
+                    editorWriter.deleteMarkdownContent(editor, from, to)
+                    break
+                }
+                case "preview": {
+                    const file = app.workspace.getActiveFile()
+                    if (!file) {
+                        new Notice('Failed to save character selection: no active file.')
+                        return
+                    }
+                    const ranges = appVaultWriter.findBlockRanges(await app.vault.read(file), codeBlockId)
+                    if (!ranges) {
+                        new Notice('Failed to delete widget: cannot find current code block range.')
+                        return
+                    }
+                    await appVaultWriter.deleteMarkdownContent(app, ranges)
+                    break
+                }
             }
-            const { from, to } = ranges
-
-            deleteMarkdownContent(editor, from, to)
         } catch (err: unknown) {
             const errMsg = err instanceof Error ? err.message : String(err)
             new Notice(`Failed to delete widget: ${errMsg}.`)
@@ -43,7 +65,7 @@ export const DeleteButton: FC<DeleteButtonProps> = ({ codeBlockId }) => {
             onClick={deleteWidget}
             aria-label="Delete widget"
         >
-            <X aria-hidden/>
+            <X aria-hidden />
         </button>
     )
 }
