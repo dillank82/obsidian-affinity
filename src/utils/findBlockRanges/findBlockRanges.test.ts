15ff27e2076c71ccba23ee92c7bea4d241c444dd
Evgeny Savelyev <dillank82@gmail.com>
2026-06-29 11:31:10 +0500

    refactor(tests): move tests from WriteService to findBlockRanges

--- src/utils/findBlockRanges/findBlockRanges.test.ts
+++ src/utils/findBlockRanges/findBlockRanges.test.ts
@@ -1,13 +1,13 @@
 import { EditorState } from "@codemirror/state"
-import { findBlockRanges } from "./WriteService"
 import { AffinityNodeData, iterateAffinityBlocks } from "utils/iterateAffinityBlocks"
+import { findBlockRangesApp, findBlockRangesEditor } from "./findBlockRanges"
 
 jest.mock('utils/iterateAffinityBlocks', () => ({
-    iterateAffinityBlocks: jest.fn((state: EditorState, callback: (data: AffinityNodeData) => void) => {})
+    iterateAffinityBlocks: jest.fn((state: EditorState, callback: (data: AffinityNodeData) => void) => { })
 }))
 const mockIterator = iterateAffinityBlocks as jest.Mock
 
-describe('findBlockRanges', () => {
+describe('findBlockRanges (editor)', () => {
     const createState = (doc: string) => EditorState.create({ doc })
 
     it('finds affinity code block ranges by id', () => {
@@ -25,7 +25,7 @@ describe('findBlockRanges', () => {
             callback({ id: id, from: 5, to: 83, toCharId: null })
         })
 
-        expect(findBlockRanges(state, id)).toStrictEqual({
+        expect(findBlockRangesEditor(state, id)).toStrictEqual({
             from: {
                 line: 1,
                 ch: 0
@@ -44,6 +44,36 @@ describe('findBlockRanges', () => {
             callback({ id: id1, from: 0, to: 7, toCharId: null })
         })
 
-        expect(findBlockRanges(state, id2)).toBeNull()
+        expect(findBlockRangesEditor(state, id2)).toBeNull()
+    })
+})
+
+describe('findBlockRanges (app)', () => {
+    it('finds affinity code block ranges by id', () => {
+        const id = '71d48b14-3840-4761-916b-2003a1877e2d'
+        const doc = [
+            '0line',
+            '1line',
+            '```affinity',
+            `  id: ${id}`,
+            '  !@#$%^&*()_+a',
+            '```',
+            '5line'
+        ].join('\n')
+        expect(findBlockRangesApp(id, doc)).toEqual({ from: 2, to: 5 })
+    })
+    it('should return null if ids mismatch', () => {
+        const id1 = '71d48b14-3840-4761-916b-2003a1877e2d'
+        const id2 = 'c9992e89-0459-44fc-b562-06df1db3c8bc'
+        const doc = [
+            '0line',
+            '1line',
+            '```affinity',
+            `  id: ${id2}`,
+            '  !@#$%^&*()_+a',
+            '```',
+            '5line'
+        ].join('\n')
+        expect(findBlockRangesApp(id1, doc)).toBeNull()
     })
 })
