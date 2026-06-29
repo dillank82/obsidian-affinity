1faa3f681f3dd399fbaddebf15dcce125ff3e554
Evgeny Savelyev <dillank82@gmail.com>
2026-06-29 11:20:22 +0500

    refactor(WriteService): move findBlockRanges util to a separate file

--- src/services/WriteService/WriteService.ts
+++ src/services/WriteService/WriteService.ts
@@ -1,36 +1,10 @@
-import { EditorState } from "@codemirror/state";
 import { App, Editor, EditorPosition, Notice } from "obsidian";
 import { MarkdownCodeBlockData } from "schemas/MarkdownCodeBlockData";
 import { dataToMarkdownContent } from "utils/dataToMarkdownContent/dataToMarkdownContent";
-import { AffinityNodeData, iterateAffinityBlocks } from "utils/iterateAffinityBlocks";
+import { findBlockRangesApp, findBlockRangesEditor } from "utils/findBlockRanges/findBlockRanges";
 
 export const editorWriter = {
-    findBlockRanges: (state: EditorState, id: string): { from: EditorPosition, to: EditorPosition } | null => {
-        let ranges: { from: EditorPosition, to: EditorPosition } | null = null
-        const setRanges = (data: AffinityNodeData) => {
-            const foundedId = data.id
-            if (id === foundedId) {
-                const { from, to } = data
-                const fromLine = state.doc.lineAt(from)
-                const toLine = state.doc.lineAt(to)
-                ranges = {
-                    from: {
-                        line: fromLine.number - 1,
-                        ch: from - fromLine.from
-                    },
-                    to: {
-                        line: toLine.number - 1,
-                        ch: to - toLine.from
-
-                    }
-
-                }
-            }
-        }
-
-        iterateAffinityBlocks(state, setRanges)
-        return ranges
-    },
+    findBlockRanges: findBlockRangesEditor,
     updateMarkdownData: (editor: Editor, data: MarkdownCodeBlockData, from: EditorPosition, to: EditorPosition) => {
         const content = dataToMarkdownContent(data)
         editor.replaceRange(content, from, to)
@@ -41,17 +15,7 @@ export const editorWriter = {
 }
 
 export const appVaultWriter = {
-    findBlockRanges: (id: string, doc: string): { from: number, to: number } | null => {
-        const lines = doc.split('\n')
-
-        const fromIndex = lines.findIndex(l => l.includes(id))
-        if (fromIndex === -1) return null
-
-        const from = lines.slice(0, fromIndex).lastIndexOf('```affinity')
-        const to = lines.indexOf('```', from + 1)
-
-        return { from, to }
-    },
+    findBlockRanges: findBlockRangesApp,
     updateMarkdownData: async (app: App, ranges: { from: number, to: number }, newData: MarkdownCodeBlockData): Promise<void> => {
         const file = app.workspace.getActiveFile()
         if (!file) {
---
+++ src/utils/findBlockRanges/findBlockRanges.ts
@@ -0,0 +1,42 @@
+import { EditorState } from "@codemirror/state"
+import { EditorPosition } from "obsidian"
+import { AffinityNodeData, iterateAffinityBlocks } from "utils/iterateAffinityBlocks"
+
+export const findBlockRangesEditor = (state: EditorState, id: string): { from: EditorPosition, to: EditorPosition } | null => {
+    let ranges: { from: EditorPosition, to: EditorPosition } | null = null
+    const setRanges = (data: AffinityNodeData) => {
+        const foundedId = data.id
+        if (id === foundedId) {
+            const { from, to } = data
+            const fromLine = state.doc.lineAt(from)
+            const toLine = state.doc.lineAt(to)
+            ranges = {
+                from: {
+                    line: fromLine.number - 1,
+                    ch: from - fromLine.from
+                },
+                to: {
+                    line: toLine.number - 1,
+                    ch: to - toLine.from
+
+                }
+
+            }
+        }
+    }
+
+    iterateAffinityBlocks(state, setRanges)
+    return ranges
+}
+
+export const findBlockRangesApp = (id: string, doc: string): { from: number, to: number } | null => {
+    const lines = doc.split('\n')
+
+    const fromIndex = lines.findIndex(l => l.includes(id))
+    if (fromIndex === -1) return null
+
+    const from = lines.slice(0, fromIndex).lastIndexOf('```affinity')
+    const to = lines.indexOf('```', from + 1)
+
+    return { from, to }
+}
